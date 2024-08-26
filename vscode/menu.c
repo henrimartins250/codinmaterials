4abfa7f874df31c8a6b5891523f229256ec67ef2
henrimartins250 <henrinkcstd@gmail.com>
2024-08-26 13:42:16 -0300

    Create menu.c

---
+++ vscode/menu.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    int order;
+    char name[25];
+
+    printf("hello! whats your name?\n\n");
+    scanf("%s", name);
+
+    printf("hi %s how are you\n\n",name);
+    printf("here's the menu for our hamberguers\n\n");
+printf("1 chesseburger, \"beef patty, chesse, mayo, lettuce, toamtoes, gergelin bread\"\n\n 2 hamburguer, \"beef patty, onions, chesse, ketchup, mayo, lettuce, tomatoes,\"\n\n3 super, \"beef patty, eggs, ham, cheese, tomatoes, lettuce, mayo, ketchp, tasty sauce,\"");
+
+switch
+case 1:
+
+break;
+case 2:
+
+break;
+case 3:
+
+break;
+default:
+    return 0;
+}
