54cec12f286cf5a92aff15a2e052d918431c2d78
henrimartins250 <henrinkcstd@gmail.com>
2024-08-27 16:15:40 -0300

    Create numberlist.py

---
+++ python/numberlist.py
@@ -0,0 +1,12 @@
+nume = []
+print("crie uma sequencia aleatoria de numeros")
+nume.append(input("escreva: "))
+nume.append(input("escreva: "))
+nume.append(input("escreva: "))
+nume.append(input("escreva: "))
+print(nume)
+nume.pop(3)
+print(nume[2])
+nume.sort
+print(nume)
+nume.clear()
